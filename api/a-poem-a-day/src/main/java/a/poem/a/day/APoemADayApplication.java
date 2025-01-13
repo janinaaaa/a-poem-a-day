bb854cd7ce775800b8968d8f5a1a607639752e8e
Janina <101583496+janinaaaa@users.noreply.github.com>
2025-01-13 18:20:49 +0100

    initial api spring boot setup

---
+++ api/a-poem-a-day/src/main/java/a/poem/a/day/APoemADayApplication.java
@@ -0,0 +1,13 @@
+package a.poem.a.day;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class APoemADayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(APoemADayApplication.class, args);
+	}
+
+}
---
+++ api/a-poem-a-day/src/test/java/a/poem/a/day/APoemADayApplicationTests.java
@@ -0,0 +1,13 @@
+package a.poem.a.day;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class APoemADayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
