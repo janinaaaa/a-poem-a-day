8136fecf1b3fd5758d87162193e04d6b71ff0928
Janina <101583496+janinaaaa@users.noreply.github.com>
2025-01-20 21:00:47 +0100

    added implementation of service methods

--- api/a-poem-a-day/src/main/java/a_poem_a_day/repository/PoemRepository.java
+++ api/a-poem-a-day/src/main/java/a_poem_a_day/repository/PoemRepository.java
@@ -3,6 +3,12 @@ package a_poem_a_day.repository;
 import a_poem_a_day.model.Poem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PoemRepository extends JpaRepository<Poem, String> {
 
+    Optional<Poem> findByTitle(String title);
+
+    Optional<Poem> findByAuthor(String author);
 }
+
--- api/a-poem-a-day/src/main/java/a_poem_a_day/service/poem/PoemServiceImpl.java
+++ api/a-poem-a-day/src/main/java/a_poem_a_day/service/poem/PoemServiceImpl.java
@@ -1,12 +1,35 @@
 package a_poem_a_day.service.poem;
 
+import a_poem_a_day.model.Poem;
 import a_poem_a_day.repository.PoemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
-public class PoemServiceImpl {
+public class PoemServiceImpl implements PoemService{
     private final PoemRepository poemRepository;
 
+    @Override
+    public Optional<Poem> getPoemById(String id) {
+        return poemRepository.findById(id);
+    }
+
+    @Override
+    public Optional<Poem> getPoemByTitle(String title) {
+        return poemRepository.findByTitle(title);
+    }
+
+    @Override
+    public Optional<Poem> getPoemByAuthor(String author) {
+        return poemRepository.findByAuthor(author);
+    }
+
+    @Override
+    public Optional<List<Poem>> getRandPoems(int n) {
+        return Optional.empty();
+    }
 }
