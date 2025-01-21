fb5c72ad080b295f98c56270fbf3a68a62b99971
Janina <101583496+janinaaaa@users.noreply.github.com>
2025-01-21 16:39:07 +0100

    implementation of controller methods

--- api/a-poem-a-day/src/main/java/a_poem_a_day/controller/PoemController.java
+++ api/a-poem-a-day/src/main/java/a_poem_a_day/controller/PoemController.java
@@ -1,13 +1,46 @@
 package a_poem_a_day.controller;
 
+import a_poem_a_day.model.Poem;
+import a_poem_a_day.service.poem.PoemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController("/poem")
+@RequiredArgsConstructor
 public class PoemController {
+    private final PoemService poemService;
     @GetMapping("/{id}")
-    public String getPoemById(@PathVariable String id) {
-        return "Poem with id " + id;
+    public ResponseEntity<Poem> getPoemById(@PathVariable String id) {
+        return poemService.getPoemById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
+
+    @GetMapping("/title/{title}")
+    public ResponseEntity<Poem> getPoemByTitle(@PathVariable String title) {
+        return poemService.getPoemByTitle(title)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Poem>> getRandPoems(int n) {
+        return poemService.getRandPoems(n)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/author/{author}")
+    public ResponseEntity<List<Poem>> getPoemByAuthor(@PathVariable String author) {
+        return poemService.getPoemByAuthor(author)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+
 }
