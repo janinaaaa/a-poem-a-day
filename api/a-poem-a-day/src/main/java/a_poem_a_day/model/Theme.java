bd0c2b1578e3b762ab627be1d90b8abf4ba1465d
Janina <101583496+janinaaaa@users.noreply.github.com>
2025-01-22 18:43:51 +0100

    added theme entity class

---
+++ api/a-poem-a-day/src/main/java/a_poem_a_day/model/Theme.java
@@ -0,0 +1,24 @@
+package a_poem_a_day.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "theme")
+
+public class Theme {
+    @Id
+    @GeneratedValue(generator = "uuid")
+    private String id;
+
+    private String name;
+    private Date themeDate;
+}
