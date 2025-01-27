bf46f3a24877734a28d9a4e841ce1a5716ca007d
Janina <101583496+janinaaaa@users.noreply.github.com>
2025-01-27 17:34:05 +0100

    implemented ThemeController

--- api/a-poem-a-day/src/main/java/a_poem_a_day/controller/ThemeController.java
+++ api/a-poem-a-day/src/main/java/a_poem_a_day/controller/ThemeController.java
@@ -1,12 +1,14 @@
 package a_poem_a_day.controller;
 
+import a_poem_a_day.dto.theme.AddThemeRequest;
 import a_poem_a_day.dto.theme.ThemeDTO;
+import a_poem_a_day.dto.theme.UpdateThemeRequest;
+import a_poem_a_day.model.Theme;
 import a_poem_a_day.service.theme.ThemeService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 import java.util.List;
@@ -25,4 +27,25 @@ public class ThemeController {
     public ResponseEntity<List<ThemeDTO>> getThemeByDate(@PathVariable Date date) {
         return ResponseEntity.ok(themeService.getThemesByDate(date));
     }
+
+    @GetMapping("/date/{startDate}/{endDate}")
+    public ResponseEntity<List<ThemeDTO>> getThemeByDateRange(@PathVariable Date startDate, @PathVariable Date endDate) {
+        return ResponseEntity.ok(themeService.getThemesByDateRange(startDate, endDate));
+    }
+
+    @GetMapping("/month/{month}")
+    public ResponseEntity<List<ThemeDTO>> getThemeByMonth(@PathVariable int month) {
+        return ResponseEntity.ok(themeService.getThemesByMonth(month));
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<Theme> addTheme(@Valid @RequestBody AddThemeRequest theme) {
+        return ResponseEntity.ok(themeService.addTheme(theme));
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<Theme> updateTheme(@Valid @RequestBody UpdateThemeRequest theme) {
+        return ResponseEntity.ok(themeService.updateTheme(theme));
+    }
+
 }
