7716cfe38841e8885794327a7d3947cdc6f12313
Janina <101583496+janinaaaa@users.noreply.github.com>
2025-01-29 16:40:07 +0100

    added register request

---
+++ api/a-poem-a-day/src/main/java/a_poem_a_day/dto/user/RegisterRequest.java
@@ -0,0 +1,21 @@
+package a_poem_a_day.dto.user;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class RegisterRequest {
+    @NotBlank(message = "name is required")
+    private String name;
+    @NotBlank(message = "last name is required")
+    private String lastName;
+    @NotBlank(message = "username is required")
+    private String username;
+    @NotBlank(message = "password is required")
+    private String email;
+}
